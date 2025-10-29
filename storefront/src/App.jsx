46965fba6110bf5dcf091d96746a8f4eb6806d5f
Daniel Henrique <dadaniel.hb@gmail.com>
2025-10-29 14:27:38 -0300

    Ajustes

--- storefront/src/App.jsx
+++ storefront/src/App.jsx
@@ -1,24 +1,23 @@
+// storefront/src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import RockPage from './pages/RockPage';
-// 1. Importe a nova página
-import ProductDetailPage from './pages/ProductDetailPage'; 
-import { AuthProvider } from './context/AuthContext';
+import ProductDetailPage from './pages/ProductDetailPage';
+// 1. Remova a importação do AuthProvider daqui
+// import { AuthProvider } from './context/AuthContext'; 
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/rock" element={<RockPage />} />
-          {/* 2. Adicione a rota para a nova página */}
-          <Route path="/produto/:id" element={<ProductDetailPage />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    // 2. Remova o AuthProvider que envolvia o Router
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/rock" element={<RockPage />} />
+        <Route path="/produto/:id" element={<ProductDetailPage />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
+export default App;   
